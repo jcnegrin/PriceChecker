22eb5cb88266057ce24d54633f85501ecba75708
Juan Negrin <jcnegrin@icloud.com>
2024-02-27 00:59:14 +0100

    Add BD Schema
    Create ER Diagram and DB Tables SQL sentences

---
+++ schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE
+    IF NOT EXISTS Tienda (
+        id VARCHAR(255),
+        nombre VARCHAR(50),
+        url VARCHAR(255),
+        PRIMARY KEY (id)
+    )
+CREATE TABLE
+    IF NOT EXISTS Categoria (
+        id VARCHAR(255),
+        nombre VARCHAR(255),
+        codigoCategoria VARCHAR(255),
+        PRIMARY KEY (id),
+        FOREIGN KEY (codigoCategoria) REFERENCES Categoria (id)
+    )
+CREATE TABLE
+    IF NOT EXISTS Producto (
+        id VARCHAR(255),
+        nombre VARCHAR(255),
+        descripcion VARCHAR(255),
+        codigoTienda VARCHAR(255),
+        codigoCategoria VARCHAR(255),
+        PRIMARY KEY (id),
+        FOREIGN KEY (codigoTienda) REFERENCES Tienda (id),
+        FOREIGN KEY (codigoCategoria) REFERENCES Categoria (codigoCategoria)
+    )
+CREATE TABLE
+    IF NOT EXISTS Precio (
+        id VARCHAR(255),
+        monto FLOAT NOT NULL,
+        medida SMALLINT,
+        unidad ENUM ('Ud', 'Kg'),
+        codigoProducto VARCHAR(255),
+        FOREIGN KEY (codigoProducto) REFERENCES Producto (id)
+    )
