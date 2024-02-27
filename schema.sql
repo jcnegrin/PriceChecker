a72d3dbb951d8b4cd5c5253df70e032d6bebdf70
Juan Negrin <jcnegrin@icloud.com>
2024-02-27 15:33:46 +0100

    Update SQL Schema

--- schema.sql
+++ schema.sql
@@ -1,18 +1,34 @@
+DROP DATABASE IF EXISTS PricePicker;
+
+CREATE DATABASE IF NOT EXISTS PricePicker;
+
+USE PricePicker;
+
+DROP TABLE IF EXISTS Precio;
+
+DROP TABLE IF EXISTS Producto;
+
+DROP TABLE IF EXISTS Categoria;
+
+DROP TABLE IF EXISTS Tienda;
+
 CREATE TABLE
     IF NOT EXISTS Tienda (
         id VARCHAR(255),
         nombre VARCHAR(50),
         url VARCHAR(255),
         PRIMARY KEY (id)
-    )
+    );
+
 CREATE TABLE
     IF NOT EXISTS Categoria (
         id VARCHAR(255),
         nombre VARCHAR(255),
         codigoCategoria VARCHAR(255),
         PRIMARY KEY (id),
-        FOREIGN KEY (codigoCategoria) REFERENCES Categoria (id)
-    )
+        CONSTRAINT fk_categoria_categoria FOREIGN KEY (codigoCategoria) REFERENCES Categoria (id)
+    );
+
 CREATE TABLE
     IF NOT EXISTS Producto (
         id VARCHAR(255),
@@ -21,9 +37,10 @@ CREATE TABLE
         codigoTienda VARCHAR(255),
         codigoCategoria VARCHAR(255),
         PRIMARY KEY (id),
-        FOREIGN KEY (codigoTienda) REFERENCES Tienda (id),
-        FOREIGN KEY (codigoCategoria) REFERENCES Categoria (codigoCategoria)
-    )
+        CONSTRAINT fk_producto_tienda FOREIGN KEY (codigoTienda) REFERENCES Tienda (id),
+        CONSTRAINT fk_producto_categoria FOREIGN KEY (codigoCategoria) REFERENCES Categoria (id)
+    );
+
 CREATE TABLE
     IF NOT EXISTS Precio (
         id VARCHAR(255),
@@ -31,5 +48,13 @@ CREATE TABLE
         medida SMALLINT,
         unidad VARCHAR(20),
         codigoProducto VARCHAR(255),
-        FOREIGN KEY (codigoProducto) REFERENCES Producto (id)
-    )
+        CONSTRAINT fk_precio_producto FOREIGN KEY (codigoProducto) REFERENCES Producto (id)
+    );
+
+DESC Categoria;
+
+DESC Precio;
+
+DESC Producto;
+
+DESC Tienda;
